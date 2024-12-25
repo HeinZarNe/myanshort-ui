958e2ad54718a7d5a7710d1bf2404b36ebaa7bea
HeinZarNe <heinzarne2522005@gmail.com>
2024-12-25 18:21:24 +0630

    config change

--- src/GoogleOauth.tsx
+++ src/GoogleOauth.tsx
@@ -1,9 +1,10 @@
 import { BsGoogle } from "react-icons/bs";
+const API_URL = import.meta.env.VITE_BACKEND_API;
 
 export const GoogleOauth = () => {
   return (
     <a
-      href="http://localhost:3000/api/auth/google"
+      href={`${API_URL}auth/google`}
       className="  w-full min-w-[280px] flex items-center gap-2 justify-center font-semibold text-gray-800 py-3 border border-gray-300 mb-3 cursor-pointer bg-white rounded-lg shadow-sm hover:bg-gray-100"
     >
       <BsGoogle size={30} className="text-blue-500" />
